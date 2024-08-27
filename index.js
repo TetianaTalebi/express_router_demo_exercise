d36bce278566a96c4c49fe79530ab3e44d10f839
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-27 00:07:20 -0400

    Add app.use(/breeders/, shelterRoutes) into index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const shelterRoutes = require('./routes/shelters');
 
+app.use('/breeders/', shelterRoutes);
+
 app.listen(3000, () => {
     console.log('Serving app on local host 3000!!!')
 })
