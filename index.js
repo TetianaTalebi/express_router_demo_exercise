8651fcd8ace77cb34f0b06a535296a566fdbd87e
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-26 23:59:02 -0400

    Define const shelterRoutes in index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const shelterRoutes = require('./routes/shelters');
+
 app.listen(3000, () => {
     console.log('Serving app on local host 3000!!!')
 })
