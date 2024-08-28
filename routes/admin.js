8fce74b338cc74d6f04b0d523fb025d10fc61bb8
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-27 23:44:49 -0400

    Define several simple routes that have to be protected in routes/admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/topsecret', (req, res) => {
+    res.send('This is a top secret!!!');
+})
+
+router.get('/deleteeverything', (req, res) => {
+    res.send('Delete it all !!!!');
+})
+
 module.exports = router;
