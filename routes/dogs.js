064bd2d775aa34e6173c06334bfa0bf6938a6abb
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-27 23:21:50 -0400

    Define several simple routes for demo purposes in routes/dogs.js

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('All dogs!');
+})
+
+router.get('/:id', (req, res) => {
+    res.send('Viewing one dog!');
+})
+
+router.put('/:id/edit', (req, res) => {
+    res.send('Editing one dog!');
+})
