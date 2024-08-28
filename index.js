feb2304c87e29e504ee6b20730301ab791ec8280
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-27 23:53:43 -0400

    Add app.use(/admin, adminRoutes) into index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const adminRoutes = require('./routes/admin');
 
 app.use('/breeders', shelterRoutes);
 app.use('/dogs', dogRoutes);
+app.use('/admin', adminRoutes);
 
 app.listen(3000, () => {
     console.log('Serving app on local host 3000!!!')
