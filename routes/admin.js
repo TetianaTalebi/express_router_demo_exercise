4cdaa367e0a64687a8cc6431f0bc3b412911f9b4
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-28 00:09:00 -0400

    Assign a middleware on the router object in routes/admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.use((req, res, next) => {
+    if (req.query.isAdmin){next()}
+    res.send('Sorry, not an Admin!!!');
+})
+
 router.get('/topsecret', (req, res) => {
     res.send('This is a top secret!!!');
 })
